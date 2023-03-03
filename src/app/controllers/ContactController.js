64df4815214c78a4f205b7c250e446679ab4368a
Amon <aamonnlimaa@gmail.com>
2023-03-03 12:47:03 -0300

    new routes and update and delete methods

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -21,12 +21,56 @@ class ContactController {
     response.json(contact);
   }
 
-  store() {
+  async store(request, response) {
     // Create new contact
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    if (!name || !email || !phone || !category_id) {
+      return response.status(400).json({ error: 'You need to fill all required fields' });
+    }
+
+    const contactExists = await ContactsRepository.findByEmail(email);
+
+    if (contactExists) {
+      return response.status(400).json({ error: 'This e-mail is already in use' });
+    }
+
+    const contact = await ContactsRepository.create({
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
-  update() {
-    // Update a contact
+  async update(request, response) {
+    // update a contact
+    const { id } = request.params;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    const contactExists = await ContactsRepository.findById(id);
+
+    if (!contactExists) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response.status(400).json({ error: 'This e-mail is already in use' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
   async delete(request, response) {
@@ -40,10 +84,10 @@ class ContactController {
     }
 
     await ContactsRepository.delete(id);
+    // 204: No content
     response.sendStatus(204);
   }
 }
 
 // Singleton
-
 module.exports = new ContactController();
